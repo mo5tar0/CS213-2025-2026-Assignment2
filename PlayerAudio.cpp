1670a1b61df7be6f355b3fb66e64f04a9280c44b
Radwa1310 <radwaabdou06@gmail.com>
2025-10-21 00:36:40 +0300

    Fix 1 issues in PlayerAudio.cpp

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -42,7 +42,7 @@ bool PlayerAudio::loadFile(const juce::File& file)
                 0,
                 nullptr,
                 reader->sampleRate);
-            setSpeed(currentspeed);
+            
             return true;
         }
         return false;
@@ -102,5 +102,6 @@ float PlayerAudio::getGain() const
 {
     return transportSource.getGain();
 }
+
 
 
